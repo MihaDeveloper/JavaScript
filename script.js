bbe6a63e22524a11ed5d8198ed3311cb52aa3db3
Mikhail Alpatov <alpatov70@mail.ru>
2021-11-26 14:59:07 +0300

    Lesson 2

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-let title,
-screens,
-screenPrice,
-rollback,
-fullPrice,
-adaptive;
-alert("Hello, Everyone!");
-console.log("Lesson 1!");
+let title = "Lesson2",
+screens = "Простые , Сложные , Интерактивные",
+screenPrice = 7771,
+rollback = 33,
+fullPrice = 666666666,
+adaptive = true;
+console.log(typeof(title));
+console.log(typeof(fullPrice));
+console.log(typeof(adaptive));
+console.log(screens.length);
+console.log(screenPrice , fullPrice);
+console.log(screens.toLowerCase().split("  "));
+console.log(fullPrice*(rollback/100));
